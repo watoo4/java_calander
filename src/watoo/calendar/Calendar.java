d0a5f69378912c7cb8059e6ddb2e629dd9367106
hall <minsuk33011@gmail.com>
2022-03-22 16:45:55 +0900

    날짜 계산

--- src/watoo/calendar/Calendar.java
+++ src/watoo/calendar/Calendar.java
@@ -1,7 +1,25 @@
 package watoo.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        System.out.println("일 월 화 수 목 금 토");
+        System.out.println("--------------------");
+        System.out.println(" 1  2  3  4  5  6  7");
+        System.out.println(" 8  9 10 11 12 13 14");
+        System.out.println("15 16 17 18 19 20 21");
+        System.out.println("22 23 24 25 26 27 28");
+
+        //숫자를 입력바다 해당하는 달의 최대 일수를 출력하는 프로그램
+        Scanner sc = new Scanner(System.in);
+        System.out.println("달을 입력하세요");
+        int month = sc.nextInt();
+
+        int[] maxDays = {31, 28, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        System.out.printf("%d달은 %d일까지 있습니다.\n",month, maxDays[month-1]);
+        sc.close();
     }
 }
---
+++ src/watoo/calendar/Sum.java
@@ -0,0 +1,4 @@
+package watoo.calendar;
+
+public class Sum {
+}
