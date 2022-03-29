ba3c0700386a4a602bd01de7a9c5620fad362a85
hall <minsuk33011@gmail.com>
2022-03-29 12:47:33 +0900

    refactoring

---
+++ src/watoo/calendar/PlanItem.java
@@ -0,0 +1,33 @@
+package watoo.calendar;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class PlanItem {
+    public Date planDate;
+    public String detail;
+    public String peoples = "";
+
+    public static Date getDatefromString(String strdate) {
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("yyyy-MM-dd").parse(strdate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+    public PlanItem(String date, String detail) {
+        this.planDate = getDatefromString(date);
+        this.detail = detail;
+    }
+
+    public Date getPlanDate() {
+        return planDate;
+    }
+
+    public void addPeople(String name) {
+        peoples += name+",";
+    }
+}
